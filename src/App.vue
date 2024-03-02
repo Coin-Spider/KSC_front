8a8b7b5025cef198bb34aa777fa3e42c9599e340
Rum <215194440@qq.com>
2024-03-02 12:35:58 +0800

    基础

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <welocme_-login/>
+  </div>
+</template>
+
+<script>
+
+import {defineComponent} from "vue";
+import Welocme_Login from "@/components/welocme_Login.vue";
+
+export default defineComponent({
+  components: {Welocme_Login}
+})
+</script>
---
+++ src/components/welocme_Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="All">
+    <img src="./public/logo.png" alt="wording" style="width: 296px; height: 186px; margin-left: 625px; margin-top: 70px">
+    <div style="font-size: 55px; font-weight: bold ; color: #FF5733; margin-left: 556px; margin-top: 20px">
+      Welcome to KSC
+    </div>
+    <div style="margin-left: 740px; font-size: 25px; font-weight: bold; color: #FF5733; margin-top: 240px">
+      log in
+    </div>
+    <img src="./public/箭头%201.png" style="width: 50px; margin-left: 750px; margin-top: 10px">
+    <img src="./public/logo.png" alt="wording" style="width: 296px; height: 186px; margin-left: 625px; margin-top: 400px">
+    <input class="login_Tab">
+    <input class="login_Tab" style="margin-top: 30px">
+    <button>
+      确认
+    </button>
+    <div style="margin-left: 740px; font-size: 25px; font-weight: bold; color: #FF5733; margin-top: 240px">
+      sign up
+    </div>
+    <img src="./public/箭头%201.png" style="width: 50px; margin-left: 750px; margin-top: 10px">
+  </div>
+</template>
+<style>
+  .All{
+    height: 4000px;
+    display: flex;
+    background-color: #ffffff;
+    flex-direction: column;
+  }
+  .login_Tab{
+    display: flex;
+    width: 320px;
+    height: 42px;
+    margin-left: 606px;
+    border-radius: 10px;
+    color: white;
+    background-color: #FF5733;
+    border: 0;
+    margin-top: 55px;
+    padding-left: 10px;
+    font-size: 20px;
+  }
+  .login_Tab:focus{
+    background-color: #FF5733;
+    border: 0;
+    outline: none;
+  }
+</style>
+
+<script setup>
+</script>
